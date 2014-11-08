d2276d057cc736d760f1301649d704285a29253f
Anand Jeyahar <anand.jeyahar@gmail.com>
2014-11-08 15:58:31 +0530

    more fixes

--- sumuka/models.py
+++ sumuka/models.py
@@ -21,9 +21,10 @@ app = Flask(__name__)
 
 # Create dummy secrey key so we can use sessions
 app.config['SECRET_KEY'] = '123456790'
-
+DB_USERNAME = 'scott@localhost'
+DB_PWD = 'tiger'
 # Create in-memory database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@localhost/sumukha'%(DB_USERNAME, DB_PWD)
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -33,6 +34,10 @@ base_path = op.join(op.dirname(__file__), 'static')
 payment_type_enums = ('onetime','standing')
 
 
+DonorChildMaps = db.Table('DonorChildMap',
+            db.Column('donor_id', db.Integer, db.ForeignKey('donor.id')),
+            db.Column('child_id', db.Integer, db.ForeignKey('child.id'))
+            )
 # Create models
 class Child(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -40,7 +45,8 @@ class Child(db.Model):
     address = db.Column(db.String(100))
     pic_path = db.Column(db.String(64))
     surgery_cost = db.Column(db.Float)
-    donors = db.Column(db.String)
+    DonorChildMaps = db.relationship('DonorChildMap', secondary=DonorChildMaps,
+                        backref=db.backref('',lazy='dynamic'))
 
 
     def __init__(self, name, cost, **kwargs):
@@ -53,17 +59,16 @@ class Donor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Unicode(64))
     paid_amnt = db.Column(db.Float)
-    payment_type = db.Column('type', db.Enum(payment_type_enums))
+#    payment_type = db.Column('type', db.Enum(payment_type_enums))
     email_id = db.Column(db.String(64))
-    child = db.relation(Child, backref='sponsored_children')
 
     def __init__(self, name, donated_amnt, **kwargs):
         pass
 
-class Receipt(db.Model):
-    pass
-class Subscription(db.Model):
-    pass
+#class Receipt(db.Model):
+#    pass
+#class Subscription(db.Model):
+#    pass
 
 def get_child_funds():
     """
@@ -80,53 +85,6 @@ def get_child_funds():
 #            os.remove(op.join(base_path, target.path))
 #    except:
 #        pass
-#
-#
-## This widget uses custom template for inline field list
-#class CustomInlineFieldListWidget(RenderTemplateWidget):
-#    def __init__(self):
-#        super(CustomInlineFieldListWidget, self).__init__('field_list.html')
-#
-#
-## This InlineModelFormList will use our custom widget
-#class CustomInlineModelFormList(InlineModelFormList):
-#    widget = CustomInlineFieldListWidget()
-#
-#
-## Create custom InlineModelConverter and tell it to use our InlineModelFormList
-#class CustomInlineModelConverter(InlineModelConverter):
-#    inline_field_list_type = CustomInlineModelFormList
-#
-#
-## Customized inline form handler
-#class InlineModelForm(InlineFormAdmin):
-#    form_excluded_columns = ('path',)
-#
-#    form_label = 'Image'
-#
-#    def __init__(self):
-#        return super(InlineModelForm, self).__init__(ChildImage)
-#
-#    def postprocess_form(self, form_class):
-#        form_class.upload = fields.FileField('Image')
-#        return form_class
-#
-#    def on_model_change(self, form, model):
-#        file_data = request.files.get(form.upload.name)
-#
-#        if file_data:
-#            model.path = secure_filename(file_data.filename)
-#            file_data.save(op.join(base_path, model.path))
-#
-#
-## Administrative class
-#class ChildAdmin(ModelView):
-#    inline_model_form_converter = CustomInlineModelConverter
-#
-#    inline_models = (InlineModelForm(),)
-#
-#    def __init__(self):
-#        super(ChildAdmin, self).__init__(Child, db.session, name='Childs')
 #
 
 # Simple page to show images
